ad927c5be6d541f9415fad2cb136e5822df6049a
Jeff Riggle <JeffreyRiggle@gmail.com>
2020-05-23 15:55:01 -0400

    add return types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { encodeValue } from './encodeUtils'
 import { decodeType, decodeValue } from './decodeUtils'
 
-export const encode = (json: any) => {
+export const encode = (json: any): Buffer => {
   return encodeValue(Buffer.alloc(0), json)
 }
 
-export const decode = (stream: Buffer) => {
+export const decode = (stream: Buffer): any => {
   const type = decodeType(stream, 0)
 
   return decodeValue(stream, type.value, type.offset).value
